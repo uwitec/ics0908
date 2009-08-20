99cd5d7c059adb8c7c3ceab59b57585b483f2a68
zlz.3907 <zlz.3907@c471f720-84ed-11de-820e-6b630fd80b91>
2009-08-20 09:45:04 +0000

    测试用

---
+++ trunk/dbserver/src/com/dbserver/test/DBTest.java
@@ -0,0 +1,52 @@
+package com.dbserver.test;
+
+public class DBTest {
+	String[] tgarr = { "¼×", "ÒÒ", "±û", "¶¡", "Îì", "¼º", "¸ý", "ÐÁ", "ÈÉ", "¹ï" };
+	String[] dzarr = { "×Ó", "³ó", "Òú", "Ã®", "³½", "ËÈ", "Îç", "Î´", "Éê", "ÓÏ", "Ðç",
+			"º¥" };
+	String[] tgparr = { "ji¨£", "y¨«", "b¨«ng", "d¨©ng", "w¨´", "j¨«", "g¨¥ng", "x¨©n",
+			"r¨¦n", "gu¨«" };
+	String[] dzparr = { "z¨«", "ch¨¯u", "y¨ªn", "m¨£o", "ch¨¦n", "s¨¬", "w¨³", "w¨¨i",
+			"sh¨¥n", "y¨¯u", "x¨±", "h¨¤i" };
+
+	int[] beforeZero = { 5, 6, 1, 2, 3, 4 };
+	int[] afterZero = { 5, 4, 3, 2, 1, 6 };
+
+	public void test() {
+		int a = 0;
+		int b = 0;
+		int maxLoop = 1000;
+		do {
+			System.out.println((1000 - maxLoop + 1) + " " + tgarr[a++]
+					+ dzarr[b++]);
+			if (a == tgarr.length) {
+				a = 0;
+			}
+			if (b == dzarr.length) {
+				b = 0;
+			}
+		} while (--maxLoop == 0 || a != 0 || b != 0);
+	}
+
+	public String getHYear(int year, boolean isBeforeZero) {
+		int a = 6 - (year & 0x0f) / 2;
+		int b = (year & 0xfff0) / 10 % 6;
+		System.out.println(year & 0x000f);
+		System.out.println(Integer.toBinaryString(year));
+		System.out.println(Integer.toBinaryString(0x000f));
+		System.out.println(Integer.toBinaryString(year & 0x000f));
+		return dzarr[afterZero[(a + b) % 6] * 2 - 1];
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// new DBTest().test();
+		System.out.println(new DBTest().getHYear(2009, false));
+
+		System.out.println("end!");
+	}
+
+}
