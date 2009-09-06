8172cc2557bdc6f755084cfeaedce0f58a9b0393
zlz.3907 <zlz.3907@c471f720-84ed-11de-820e-6b630fd80b91>
2009-09-06 04:16:56 +0000

    报表bean

---
+++ Icdb/src/com/icdb/report/ABCReportBean.java
@@ -0,0 +1,165 @@
+package com.icdb.report;
+
+public class ABCReportBean {
+
+	private String name;
+	private String materielType;
+	private String specification;
+	private String util;
+
+	private int lastAmount;
+	private double lastPrice;
+	private double lastTotalPrice;
+
+	private int amount;
+	private double price;
+	private double totalPrice;
+
+	private int inAmount;
+	private double inPrice;
+	private double inTotalPrice;
+
+	private int outAmount;
+	private double outPrice;
+	private double outTotalPrice;
+
+	private String remark;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMaterielType() {
+		return materielType;
+	}
+
+	public void setMaterielType(String materielType) {
+		this.materielType = materielType;
+	}
+
+	public String getSpecification() {
+		return specification;
+	}
+
+	public void setSpecification(String specification) {
+		this.specification = specification;
+	}
+
+	public String getUtil() {
+		return util;
+	}
+
+	public void setUtil(String util) {
+		this.util = util;
+	}
+
+	public int getLastAmount() {
+		return lastAmount;
+	}
+
+	public void setLastAmount(int lastAmount) {
+		this.lastAmount = lastAmount;
+	}
+
+	public double getLastPrice() {
+		return lastPrice;
+	}
+
+	public void setLastPrice(double lastPrice) {
+		this.lastPrice = lastPrice;
+	}
+
+	public double getLastTotalPrice() {
+		return lastTotalPrice;
+	}
+
+	public void setLastTotalPrice(double lastTotalPrice) {
+		this.lastTotalPrice = lastTotalPrice;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public double getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	public int getInAmount() {
+		return inAmount;
+	}
+
+	public void setInAmount(int inAmount) {
+		this.inAmount = inAmount;
+	}
+
+	public double getInPrice() {
+		return inPrice;
+	}
+
+	public void setInPrice(double inPrice) {
+		this.inPrice = inPrice;
+	}
+
+	public double getInTotalPrice() {
+		return inTotalPrice;
+	}
+
+	public void setInTotalPrice(double inTotalPrice) {
+		this.inTotalPrice = inTotalPrice;
+	}
+
+	public int getOutAmount() {
+		return outAmount;
+	}
+
+	public void setOutAmount(int outAmount) {
+		this.outAmount = outAmount;
+	}
+
+	public double getOutPrice() {
+		return outPrice;
+	}
+
+	public void setOutPrice(double outPrice) {
+		this.outPrice = outPrice;
+	}
+
+	public double getOutTotalPrice() {
+		return outTotalPrice;
+	}
+
+	public void setOutTotalPrice(double outTotalPrice) {
+		this.outTotalPrice = outTotalPrice;
+	}
+
+	public String getRemark() {
+		return remark;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	
+	
+}
