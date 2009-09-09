dc85655b80580cbbc8eec4723f164331eeb46de2
zlz.3907 <zlz.3907@c471f720-84ed-11de-820e-6b630fd80b91>
2009-09-09 09:48:33 +0000

    报表页面

--- Icdb/src/com/action/report/ABCReportAction.java
+++ Icdb/src/com/action/report/ABCReportAction.java
@@ -1,5 +1,11 @@
 package com.action.report;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.dbserver.DBServer;
+import com.mydomain.bean.report.ABCReportBean;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class ABCReportAction extends ActionSupport {
@@ -8,7 +14,52 @@ public class ABCReportAction extends ActionSupport {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	
+
+	private List<ABCReportBean> abcReportList;
+
+	private ABCReportBean abcReportBean;
+
+	public String showReportList() {
+		try {
+			abcReportList = new ArrayList<ABCReportBean>();
+			if (null == abcReportBean) {
+				abcReportBean = new ABCReportBean();
+			}
+			//abcReportBean.setBeginDate("2009-09-01 00:00:00");
+			//abcReportBean.setEndDate("2009-09-08 23:59:59");
+			// System.out.println("abcbean: " + abcReportBean.getBeginDate());
+			// System.out.println("abcbean: " + abcReportBean.getEndDate());
+			List<?> list = DBServer.quider.queryForList(
+					"selectAllABCReportBean", abcReportBean);
+			if (null != list) {
+				for (int i = 0; i < list.size(); i++) {
+					Object o = list.get(i);
+					if (o instanceof ABCReportBean) {
+						abcReportList.add((ABCReportBean) o);
+					}
+				}
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "SUCCESS";
+	}
+
+	public List<ABCReportBean> getAbcReportList() {
+		return abcReportList;
+	}
+
+	public void setAbcReportList(List<ABCReportBean> abcReportList) {
+		this.abcReportList = abcReportList;
+	}
+
+	public ABCReportBean getAbcReportBean() {
+		return abcReportBean;
+	}
+
+	public void setAbcReportBean(ABCReportBean abcReportBean) {
+		this.abcReportBean = abcReportBean;
+	}
 
 }
