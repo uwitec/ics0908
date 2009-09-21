62d6f642ee3cfe48ff00956e74ef820acd695c6b
zlz.3907 <zlz.3907@c471f720-84ed-11de-820e-6b630fd80b91>
2009-09-21 08:47:25 +0000

    目录XML文件读取

---
+++ trunk/Icdb/src/com/manage/permission/MenuHandler.java
@@ -0,0 +1,94 @@
+package com.manage.permission;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import com.mydomain.bean.permission.Item;
+import com.mydomain.bean.permission.Menu;
+
+public class MenuHandler extends DefaultHandler {
+	private Map<String, Menu> ims = null;
+	private boolean isFilter = false;
+	private Menu menu = null;
+
+	private Item item = null;
+
+	private String keepName = null;
+
+	public void setKeepName(String keepName) {
+		this.keepName = keepName;
+	}
+
+	public Map<String, Menu> getIms() {
+		return ims;
+	}
+
+	@Override
+	public void startDocument() throws SAXException {
+		if (null == this.ims) {
+			ims = new HashMap<String, Menu>();
+		}
+	}
+
+	@Override
+	public void startElement(String uri, String localName, String qName,
+			Attributes attributes) throws SAXException {
+		if ("menu".equalsIgnoreCase(qName)) {
+			if (null != attributes) {
+				String name = attributes.getValue("name");
+				if (null != keepName && keepName.equalsIgnoreCase(name)) {
+					this.menu = new Menu();
+					this.menu.setName(name);
+					this.menu.setCode(attributes.getValue("code"));
+					this.menu.setItemList(new ArrayList<Item>());
+					ims.put(this.menu.getName(), this.menu);
+				} else {
+					isFilter = true;
+				}
+			}
+		}
+		if ("item".equalsIgnoreCase(qName)) {
+			if (!isFilter) {
+				if (null != attributes) {
+					Item aItem = new Item();
+					aItem.setCode(attributes.getValue("code"));
+					String pIndex = attributes.getValue("permissionIndex");
+					String level = attributes.getValue("level");
+					Integer i = 0;
+					Integer lev = 0;
+					try {
+						i = Integer.parseInt(pIndex);
+						lev = Integer.parseInt(level);
+					} catch (NumberFormatException e) {
+						i = -1;
+						lev = -1;
+					}
+					aItem.setPermissionIndex(i);
+					aItem.setLevel(lev);
+					aItem.setViewName(attributes.getValue("viewName"));
+					aItem.setSubItemList(new ArrayList<Item>());
+
+					if (null != this.item
+							&& this.item.getLevel() > aItem.getLevel()) {
+						this.item.getSubItemList().add(aItem);
+					} else if (null != this.menu) {
+						this.menu.getItemList().add(aItem);
+					}
+				}
+			}
+		}
+	}
+
+	@Override
+	public void endElement(String uri, String localName, String qName)
+			throws SAXException {
+		if ("menu".equalsIgnoreCase(qName)) {
+			isFilter = false;
+		}
+	}
+}
---
+++ trunk/Icdb/src/com/manage/permission/MenuReader.java
@@ -0,0 +1,46 @@
+package com.manage.permission;
+
+import java.io.File;
+import java.util.Map;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import com.dbserver.conf.Environment;
+import com.mydomain.bean.permission.Menu;
+
+public class MenuReader {
+
+	private static Map<String, Menu> ims = null;
+
+	public static Menu readMenu(String name) {
+		if (null != name) {
+			if (null == ims) {
+				String filename = null;
+				filename = Environment.getContext() + "/menu.xml";
+				SAXParserFactory spf = SAXParserFactory.newInstance();
+				SAXParser saxParser = null;
+				MenuHandler mr = new MenuHandler();
+				mr.setKeepName(name);
+				try {
+					saxParser = spf.newSAXParser();
+					saxParser.parse(new File(filename), mr);
+					ims = mr.getIms();
+				} catch (Exception ex) {
+					System.err.println(ex);
+					System.exit(1);
+				}
+			}
+			return ims.get(name);
+		}
+		return null;
+	}
+
+	public MenuReader() {
+	}
+
+	public static void main(String[] args) {
+		System.out.println(readMenu("userMenu").viewMenu());
+	}
+
+}
